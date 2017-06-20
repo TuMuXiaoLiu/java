01cf097f4bdf8ba89172c8c28dbbf7921042377f
TuMuXiaoLiu <chunlincao@foxmail.com>
2017-06-20 20:00:38 +0800

    添加前置通知等组件，编写application.xml文件，但还有问题，虽然配置完了，但前置通知的方法未被调用

---
+++ SpringDemo1/src/com/chinasofti/ccl/aoptest/AopBefore.java
@@ -0,0 +1,20 @@
+package com.chinasofti.ccl.aoptest;
+
+import org.aspectj.lang.JoinPoint;
+
+/**
+ * @author ChulinCao E-mail: caochunlin@chinasofti.com
+ * @date 2017年6月20日 下午7:10:30
+ * @version 1.8
+ * @since
+ * @parameter
+ * @PS
+ */
+public class AopBefore {
+
+	public void beforetest(JoinPoint joinPoint) {
+		System.out.println(11);
+		System.out.println(joinPoint.getTarget().getClass().getName());
+	}
+
+}
